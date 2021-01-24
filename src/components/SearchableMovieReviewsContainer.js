69842535364b520172f9f911244b0e9baceab747
David Strauch <dmaster18@gmail.com>
2021-01-24 04:02:03 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,84 +1,49 @@
-// Code SearchableMovieReviewsContainer Here
-import React from 'react';
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
 import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const API_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-    + `api-key=${NYT_API_KEY}&query=`;
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`;
 
-function urlForSearch(term) {
-    return `${API_URL}${term}`
-}
+class SearchableMovieReviewsContainer extends Component {
+  state = {
+    searchTerm: '',
+    reviews: []
+  };
 
-const SearchingWidget = ({searchTerm}) => (
-    <p>
-        {searchTerm ?
-            'Searching for ' + searchTerm
-            : ''}
-    </p>
-);
-/**
- * Make an api call for movie reviews
- * @param {string} searchTerm term to search for
- */
-function apiSearch(searchTerm) {
-    return fetch(urlForSearch(searchTerm))
-        .then(results => results.json())
-}
+  handleSearchInputChange = event =>
+    this.setState({ searchTerm: event.target.value });
 
-class SearchableMovieReviewsContainer extends React.Component {
-    constructor(props) {
-        super(props);
+  handleSubmit = event => {
+    event.preventDefault();
 
-        this.state = {
-            reviews: [],
-            searchTerm: ''
-        };
-    }
+    fetch(BASE_URL.concat(this.state.searchTerm))
+      .then(res => res.json())
+      .then(res => this.setState({ reviews: res.results }));
+  };
 
-    // API call with search terms 
-    fetchResults(searchTerm) {
-        apiSearch(searchTerm)
-            .then(reviews => {
-                this.setState({ reviews: reviews.results })
-            })
-    }
-
-    //User submits search term, app should re-render
-    updateReviews(searchTerm) {
-        this.setState({
-            searchTerm,
-            reviews: []
-        });
-        this.fetchResults(searchTerm);
-    }
-
-    render() {
-        let searchWidget = '';
-        let reviewsWidget =  '';
-        if (this.state.searchTerm && this.state.reviews.length === 0) {
-            searchWidget = <SearchingWidget searchTerm={this.state.searchTerm} />
-        }
-        else if (this.state.reviews.length > 0) {
-            reviewsWidget = (
-            <div>
-                <h1>Reviews matching your search term: </h1>
-                <MovieReviews reviews={this.state.reviews} />
-            </div>
-            );
-        }
-        return (
-            <div className="searchable-movie-reviews">
-                Search reviews: <input id="searchTerm" type="text" />
-                <input type="submit" onClick={evt => {
-                    let searchTerm = document.getElementById('searchTerm').value;
-                    this.updateReviews(searchTerm.trim());
-                }} />
-                {searchWidget}
-                {reviewsWidget}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search-input">Search Movie Reviews</label>
+          <input
+            id="search-input"
+            type="text"
+            style={{ width: 300 }}
+            onChange={this.handleSearchInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {typeof this.state.reviews === 'object' &&
+          this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
 }
 
 export default SearchableMovieReviewsContainer;
