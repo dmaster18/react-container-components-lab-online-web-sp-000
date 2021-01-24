b599ed3036997136db31a0565d720ccadecefeb8
David Strauch <dmaster18@gmail.com>
2021-01-24 04:01:31 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,36 +7,44 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+        super()
 
-class LatestMovieReviewsContainer extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        reviews: []
-      };
+        this.state = {
+            reviews: []
+        }
     }
-  
-    fetchReviews = () => {
-      fetch(URL)
-      .then(results => results.json())
-      .then(reviews => {
-        this.setState({reviews: reviews.results})
-      })
-    };
-  
-    componentDidMount(){
-      this.fetchReviews();
+
+    componentDidMount() {
+        fetch(URL)
+            .then(response => response.json())
+            .then(response => this.setState({reviews: response.results}))
     }
-  
+
+    // componentDidMount() {
+    //     fetch(URL)
+    //       .then(res => res.json())
+    //       .then(response => this.setState({ reviews: response.results }));
+    //   }
+    
     render() {
-      return (
-          <div className="latest-movie-reviews">
-            <h1>Latest reviews</h1>
-            <MovieReviews reviews={this.state.reviews}/>
-          </div>
-      );
+        return (
+            <div className="latest-movie-reviews">
+                <h2>The Latest Reviews:</h2>
+                <MovieReviews reviews={this.state.reveiws} />
+            </div>
+        )
     }
-  }
-  
-  export default LatestMovieReviewsContainer;
+
+    render() {
+        return (
+          <div className="latest-movie-reviews">
+            <h2>The Latest Reviews:</h2>
+            <MovieReviews reviews={this.state.reviews} />
+          </div>
+        );
+      }
+}
+
+export default LatestMovieReviewsContainer;
