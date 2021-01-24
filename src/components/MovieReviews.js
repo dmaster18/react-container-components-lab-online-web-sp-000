b932938a18591f1b2357e539866273572bb56613
David Strauch <dmaster18@gmail.com>
2021-01-24 04:01:48 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,43 +1,36 @@
-// Code MovieReviews Here
-// Code MovieReviews Here
-
 import React from 'react';
 
-const Review = ({ byline, headline, summary_short }) => {
-    return (
-        <div className="review">
-            <li>
-            <h3>{headline}</h3>
-            <h4>By: {byline}</h4>
-            <p>{summary_short}</p>
-            </li>
-        </div>
-    )
-}
+const Review = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
 
-const MovieReviews = ({ reviews }) => {
-    let i = 0;
-    // if (reviews.length === 0) throw 'WTF?'
-    return (
-        <div className="review-list">
-            <ul>
-            {reviews.map(review => 
-                <div className="review">
-                    <li>
-                    <h3>{review.headline}</h3>
-                    <h4>By: {review.byline}</h4>
-                    <p>{review.summary_short}</p>
-                    </li>
-                </div>
-        )
-            }
-            </ul>
-        </div>
-    )
+    <div
+      key={headline}
+      className="review"
+    >
+      <header>
+        <a
+          className="review-link"
+          href={link.url}
+        >
+          {headline}
+        </a>
+        <br/>
+        <span className="author">{byline}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
+    </div>
+  );
 };
 
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
 MovieReviews.defaultProps = {
-    reviews: []
-}
-  
-  export default MovieReviews;
+  reviews: []
+};
+
+export default MovieReviews;
